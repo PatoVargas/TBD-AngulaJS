86bd8aa648608659b2c96e9722ebbb94af26038c
PatoVargas <patricio.vargas@usach.cl>
2015-05-22 19:58:11 -0300

    popup para las fotos, falta mejorarlo

--- js/demo-app.js
+++ js/demo-app.js
@@ -36,6 +36,80 @@
         
     });
 
+    app.config( [
+        '$compileProvider',
+        function( $compileProvider )
+        {   
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+            // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+        }
+    ]);
+
+    app.directive('modalDialog', function($window, $templateCache, $compile, $http) {
+      return {
+        restrict: 'EA',
+        scope: {
+          show: '=',
+          modalUser: '=',
+          saveUser: '&',
+          templateUser: '@'
+        },
+        replace: true, // Replace with the template below
+        //transclude: true, // we want to insert custom content inside the directive
+        link: function(scope, element, attrs) {
+
+          $http.get(scope.templateUser, {cache: $templateCache}).success(function(tplContent){
+                    element.replaceWith($compile(tplContent)(scope));                
+                  });
+                  
+          scope.dialogStyle = {};
+          if (attrs.width) {
+            scope.dialogStyle.width = attrs.width + '%';
+            scope.dialogStyle.left = ( ( 100 - attrs.width ) / 2 ) + '%';
+          }
+          if (attrs.height) {
+            scope.dialogStyle.height = attrs.height + '%';
+            scope.dialogStyle.top = ( ( 100 - attrs.height ) / 2 ) + '%';
+          }
+            
+          scope.hideModal = function() {
+            scope.show = false;
+          };
+
+          scope.clone = function(obj) {
+            if (obj === null || typeof obj !== 'object') {
+                return obj;
+            }
+            var temp = obj.constructor(); // give temp the original obj's constructor
+            for (var key in obj) {
+                temp[key] = scope.clone(obj[key]);
+            }
+            return temp;
+          };
+
+          var tempUser = scope.clone(scope.modalUser);
+          
+          scope.save = function() {
+            scope.saveUser(scope.modalUser);
+            scope.show = false;
+          };
+          
+          scope.cancel = function() {
+            scope.modalUser = scope.clone(tempUser);
+            scope.show = false;
+          };
+        }
+      };
+    });
+
+    app.controller('popupCtrl', function($scope, $window) {
+      $scope.modalShown = false;
+      $scope.userMod = {};
+      $scope.toggleModal = function() {
+        $scope.modalShown = !$scope.modalShown;
+      }
+    });
+
     app.factory('loginService',function($http, sessionService){
         return{
             login:function(user,scope){
@@ -88,6 +162,15 @@
 			store.camaras = data;
 		});	
 	}]);
+
+    app.controller('ComentariosCtrl', [ '$http', function($http) {
+        var store = this;
+        store.comentarios = [];
+        
+        $http.get('JSON/comentarios.json').success(function(data){
+            store.comentarios = data;
+        }); 
+    }]);
 
 	var app = angular.module('appServices', []);
 
